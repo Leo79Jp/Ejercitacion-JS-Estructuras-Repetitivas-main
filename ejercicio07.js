39606b61938a0302b1c9fb435207b60f1b9161f5
Leo79Jp <leojparodi@gmail.com>
2025-04-07 18:45:35 -0300

    Complete ejercicios 07-08

--- ejercicio07.js
+++ ejercicio07.js
@@ -1,2 +1,15 @@
 // Ejercicio 7
 // Realizar la operación de potenciación (a^b), de dos valores enteros positivos, con multiplicaciones.
+const prompt = require('prompt-sync')({ siging: true})
+
+let A = parseInt(prompt(`Ingrese el valor de ' A ': `))
+let B = parseInt(prompt(`Ingrese el valor de ' B ': `))
+let acumulador = A
+
+console.log(`EL valor de ( A = ${A} ) elevado a el valor de ( B = ${B} ) es igual a : ${A**B}`);
+
+if ( A ** B){
+    for (let i = B; i > 1; i--) {
+        console.log(`A * A  = ${acumulador} * ${A} = ${acumulador*=A}`);
+    }
+}
--- ejercicio08.js
+++ ejercicio08.js
@@ -1,2 +1,13 @@
 // Ejercicio 8
 // Obtener el resto de la división entera a%b de dos números enteros positivos mediante restas.
+
+const prompt = require('prompt-sync')({ siging: true})
+
+let A = parseInt(prompt(`Ingrese el valor de ' A ': `))
+let B = parseInt(prompt(`Ingrese el valor de ' B ': `))
+let resto = (A%B);
+
+for (let i = A; i > 0; i-=B) {
+    console.log(i);
+}
+console.log({resto});
